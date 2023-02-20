f9b24d79a6d508f03a901d141e13b4e451200d72
MichurinDev <m14ur1nak@yandex.ru>
2023-02-20 17:30:43 +0700

    Пофикшен спуск

--- Assets/USERS_FILE/Scripts/PlayerMove.cs
+++ Assets/USERS_FILE/Scripts/PlayerMove.cs
@@ -14,7 +14,8 @@ public class PlayerMove : MonoBehaviour
     public OnJoinedRoom OnJoinedRoom;
     public FinStatSync sync;
     public FinishStatistic FinishStatistic;
-    GameObject finPivot;
+    public GameObject finPivot;
+
 
     private void FixedUpdate()
     {
@@ -43,6 +44,12 @@ public class PlayerMove : MonoBehaviour
             if (Camera.localRotation.w > 0)
             {
                 rb.velocity = new Vector3(Camera.localRotation.z * rb.velocity.z * 5 * rb.velocity.z / 50, rb.velocity.y, rb.velocity.z);
+                if (rb.velocity.x >= 45f)
+                {
+                    
+                    rb.velocity = new Vector3(45, rb.velocity.y, rb.velocity.z);
+                    
+                }
                 ski.transform.localRotation = new Quaternion(ski.transform.localRotation.x, -Camera.localRotation.z, ski.transform.localRotation.z, ski.transform.localRotation.w);
             }
             if (Camera.localRotation.w < 0)
